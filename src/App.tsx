5134d1a61d3977bc4ef04d2df72b0ef23defa6f6
july2m3 <july2m3@gmail.com>
2020-07-08 21:37:52 -0500

    Basic setup Portfolio is a centered card with quick info, pic and links

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+import me from './pic.jpg';
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="container">
+        <div className="info">
+          <h1>Julio Fuentes</h1>
+          <h2>Full Stack Dev</h2>
+        </div>
+        <div className="image">
+          <img src={me} alt="me" />
+        </div>
+        <div className="social">
+          <ul className="links">
+            <li>
+              <span role="img" aria-label="happy">
+                😬
+              </span>
+            </li>
+            <li>
+              <span role="img" aria-label="happy">
+                😬
+              </span>
+            </li>
+            <li>
+              <span role="img" aria-label="happy">
+                😬
+              </span>
+            </li>
+          </ul>
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
