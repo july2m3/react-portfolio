22d6720a359cac7307ca99a9ab66d7980f729e22
july2m3 <july2m3@gmail.com>
2020-07-15 17:36:01 -0500

    Updates

---
+++ precache-manifest.38299fd43e4293311d7915695e60ba91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09fe54ca6c7a63c24165f7067c2c4a1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "c1e9b755cb9bda73a417",
+    "url": "/react-portfolio/static/css/main.c3547fa7.chunk.css"
+  },
+  {
+    "revision": "467297070f9e0ec57fb5",
+    "url": "/react-portfolio/static/js/2.95b021ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.95b021ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e9b755cb9bda73a417",
+    "url": "/react-portfolio/static/js/main.e2d8932a.chunk.js"
+  },
+  {
+    "revision": "0346f5e54be923396aed",
+    "url": "/react-portfolio/static/js/runtime-main.8a7d631b.js"
+  },
+  {
+    "revision": "41406fbf410adb33cb84d2603f48c656",
+    "url": "/react-portfolio/static/media/pic.41406fbf.jpg"
+  }
+]);
