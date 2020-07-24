618b927b2bc72f6a9b69382674d2271a4ce91c85
july2m3 <july2m3@gmail.com>
2020-07-24 17:10:07 -0500

    Added a menu button for mobile layout

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,22 +3,25 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navigation">
-      <ul>
-        <li>
-          <a href="#header">Home</a>
-        </li>
-        <li>
-          <a href="https://docs.google.com/document/d/1wNUELDJT3cnT8ZcbMKjAT5eYwflslrx9ZfXofKFCp54/edit?usp=sharing">
-            Resume
-          </a>
-        </li>
-        <li>
-          <a href="#portfolio">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      <div className="dropdown">
+        <span className="menu">Menu</span>
+        <ul>
+          <li>
+            <a href="#header">Home</a>
+          </li>
+          <li>
+            <a href="https://docs.google.com/document/d/1wNUELDJT3cnT8ZcbMKjAT5eYwflslrx9ZfXofKFCp54/edit?usp=sharing">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a href="#portfolio">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
