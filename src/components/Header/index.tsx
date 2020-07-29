e6212a594747a672a3fef818e401341891b00f7c
Julio <july2m3@gmail.com>
2020-07-28 20:12:29 -0500

    Mobile menu now spans entire width

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,20 @@
-import React from "react";
+import React from 'react';
 
-import me from "./pic.jpg";
+import './style.scss';
+import me from './pic.jpg';
 
 function Header() {
   return (
-    <header className="header" id="header">
+    <header className="hero" id="header">
       <div className="info">
         <h1>
           Hi, I'm: <span className="important name">Julio Fuentes</span>
         </h1>
         <p>
-          I am an aspiring full stack{" "}
+          I am an aspiring full stack{' '}
           <span className="important">javascript developer </span>
           in Milwaukee Wisconsin . I am always on the lookout for new
-          opportunities to learn and grow. Don't be a stranger{" "}
+          opportunities to learn and grow. Don't be a stranger{' '}
           <span role="img" aria-label="content">
             😊
           </span>
@@ -24,8 +25,8 @@ function Header() {
       </div>
 
       <div className="image">
-        {" "}
-        <img src={me} alt="me" />{" "}
+        {' '}
+        <img src={me} alt="me" />{' '}
       </div>
 
       <div className="scroll-cue">
