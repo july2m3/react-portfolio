41937734d09cf5114a5f95a39b9cf884f05d76bc
july2m3 <july2m3@gmail.com>
2020-07-29 11:37:20 -0500

    Changed header to use CSS grid

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { ReactComponent as Me } from './me.svg';
+import { ReactComponent as Me } from "./me.svg";
 
 function Header() {
   return (
     <header className="hero" id="header">
       <div className="info">
         <h1>
-          Hi, I'm: <span className="important name">Julio Fuentes</span>
+          Hi, I'm, <span className="important name">Julio Fuentes</span>
         </h1>
         <p>
-          I am an aspiring full stack{' '}
+          I am an aspiring full stack{" "}
           <span className="important">javascript developer </span>
           in Milwaukee Wisconsin . I am always on the lookout for new
-          opportunities to learn and grow. Don't be a stranger{' '}
+          opportunities to learn and grow. Don't be a stranger{" "}
           <span role="img" aria-label="content">
             😊
           </span>
@@ -21,7 +21,7 @@ function Header() {
       </div>
 
       <div className="image">
-        {' '}
+        {" "}
         {/* <img src={me} alt="me" />{' '} */}
         {/* <svg src={Me} /> */}
         <Me />
